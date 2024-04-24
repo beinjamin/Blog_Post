315fdd6e9d17308d7a1c230d2384969b8071e94b
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-04-24 15:51:58 +0100

    Finalisation avec le model

--- database/migrations/2024_04_22_203257_create_posts_table.php
+++ database/migrations/2024_04_22_203257_create_posts_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('excerpt');
+            $table->text('content');
+            $table->string('trumbanail');
             $table->timestamps();
+           
         });
     }
 
