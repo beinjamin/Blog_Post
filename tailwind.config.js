65157928ec7e462ab31ac7b2fe6255118a7490c0
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-04-25 12:22:17 +0100

    finalisation  installation tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'class', // only generate classes
+    }),
   ],
 }
 
