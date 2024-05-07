4a8e7faf107e58145849c372d27b44fc4ec2747b
Beinjamin <jauresbeinjamin@gmail.com>
2024-05-07 17:07:07 +0100

    update

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased pt-10 pb-16 md:pb-32">
+    
     {{-- Conteneur global --}}
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         {{-- Header --}}
