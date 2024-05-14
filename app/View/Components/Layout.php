eb0775e6d07a7bf41eeebe0cfc3ad24ada5cc78b
Beinjamin <jauresbeinjamin@gmail.com>
2024-05-14 16:22:35 +0100

    Mise en place layotu

--- app/View/Components/Layout.php
+++ app/View/Components/Layout.php
@@ -21,6 +21,6 @@ class Layout extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.layout');
+        return view('layouts.default');
     }
 }
