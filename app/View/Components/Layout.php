ea6eea17ecf81805ce5d66bcef58c828670a6804
Beinjamin <jauresbeinjamin@gmail.com>
2024-05-14 15:23:37 +0100

    Ecriture logique Layout

--- app/View/Components/Layout.php
+++ app/View/Components/Layout.php
@@ -11,9 +11,9 @@ class Layout extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(public string $title ='')
     {
-        //
+        $this->title = config('app.name') . ($title ? "| $title":"");
     }
 
     /**
